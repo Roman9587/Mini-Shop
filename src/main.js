46c3e6ff2a0a35e0eba6827c66ee17ff7aca05f8
Roman9587 <rkim2972@gmail.com>
2021-02-09 11:06:18 +0600

    установили firebase

--- src/main.js
+++ src/main.js
@@ -3,7 +3,23 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+
+
 Vue.config.productionTip = false
+
+let firebaseConfig = {
+  apiKey: "AIzaSyBzolzkmtXfcs1Xa1T_1h_qieavqUIHTWY",
+  authDomain: "mini-shop-300a9.firebaseapp.com",
+  projectId: "mini-shop-300a9",
+  storageBucket: "mini-shop-300a9.appspot.com",
+  messagingSenderId: "813718231393",
+  appId: "1:813718231393:web:831cfe1115c12237ba8d20"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   vuetify,
--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>LoGin</div>
+  <v-form @submit.prevent='submitHandler'>
+    <v-container v-flex>
+      <v-text-field label='E-mail' v-model='email'></v-text-field>
+      <v-text-field label='Password' v-model='password'></v-text-field>
+      <v-checkbox v-model='checkbox' label='Запомнить меня'></v-checkbox>
+      <v-btn type='submit' color='success'>Войти</v-btn>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+
+  methods: {
+    submitHandlers,
+  },
+}
 </script>
 
 <style>
